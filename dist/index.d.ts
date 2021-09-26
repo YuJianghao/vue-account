67b563c62bca1ff4f999ebe962cca95d187818da
winwin2011 <yjh2011@live.com>
2021-09-27 06:33:18 +0800

    feat: vue-simple-account

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,47 @@
-export declare function hello(name: string): void;
+import { App, InjectionKey } from "vue";
+import { AxiosInstance } from "axios";
+import { RouteLocationNormalized } from "vue-router";
+interface IConfig {
+    baseURL: string;
+    authBaseURL?: string;
+    onTokenExpire: () => void;
+    prefix?: string;
+}
+interface IUserInfo {
+    username: string;
+}
+declare class Account {
+    private baseURL;
+    private authBaseURL;
+    private onTokenExpire;
+    private storage;
+    origin: AxiosInstance;
+    access: AxiosInstance;
+    refresh: AxiosInstance;
+    constructor(config: IConfig);
+    install(vue: App, injectKey?: InjectionKey<Account> | string): void;
+    get defaults(): {
+        baseURL: string;
+        headers: {
+            "Content-Type": string;
+        };
+    };
+    private withAuthBase;
+    private setupAccess;
+    private setupRequest;
+    get isSignedIn(): boolean;
+    signin(username: string, password: string): Promise<any>;
+    signout(): Promise<void>;
+    info(): Promise<IUserInfo>;
+    changeInfo(info?: {
+        username?: string;
+        password?: string;
+    }): Promise<void>;
+    beforeEachGuard(config: {
+        home: string;
+        signin: string;
+    }): (to: RouteLocationNormalized, from: RouteLocationNormalized) => string | true;
+}
+export declare function createAccount(config: IConfig): Account;
+export declare function useAccount(key?: InjectionKey<Account> | string | null): Account | undefined;
+export {};
--- rollup.config.js
+++ rollup.config.js
@@ -2,29 +2,19 @@ import clear from "rollup-plugin-clear";
 import typescript from "rollup-plugin-typescript2";
 import { terser } from "rollup-plugin-terser";
 import filesize from "rollup-plugin-filesize";
+
 export default {
   input: "./index.ts",
   output: [
     {
       file: "dist/index.js",
       format: "cjs",
-      // for vue lib
-      // globals: {
-      //   vue: "Vue",
-      // },
     },
     {
       file: "dist/index.esm.js",
       format: "esm",
-      // for vue lib
-      // globals: {
-      //   vue: "Vue",
-      // },
     },
   ],
   plugins: [clear({ targets: ["dist"] }), typescript(), terser(), filesize()],
-  external: [
-    // for vue lib
-    // "vue",
-  ],
+  external: ["vue", "vue-router", "axios"],
 };
